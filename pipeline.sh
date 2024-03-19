f1ce7075ce8a02b67df628f4ddff458ec9faee43
tirumalareddysanampudi <45302057+tirumalareddysanampudi@users.noreply.github.com>
2024-03-19 19:46:15 +0530

    Update pipeline.sh

--- pipeline.sh
+++ pipeline.sh
@@ -37,12 +37,12 @@ pipeline{
                 sh 'sudo apt-get install npm -y'
             }
         }
-        stage('OWASP FS SCAN') {
-            steps {
-                dependencyCheck additionalArguments: '--scan ./ --disableYarnAudit --disableNodeAudit', odcInstallation: 'DP-Check'
-                dependencyCheckPublisher pattern: '**/dependency-check-report.xml'
-            }
-        }
+        # stage('OWASP FS SCAN') {
+        #     steps {
+        #         dependencyCheck additionalArguments: '--scan ./ --disableYarnAudit --disableNodeAudit', odcInstallation: 'DP-Check'
+        #         dependencyCheckPublisher pattern: '**/dependency-check-report.xml'
+        #     }
+        # }
         stage('TRIVY FS SCAN') {
             steps {
                 sh "trivy fs . > trivyfs.txt"
