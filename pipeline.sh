178835329d1af5c0acafaec6fcd8939f092e9ca6
tirumalareddysanampudi <45302057+tirumalareddysanampudi@users.noreply.github.com>
2024-03-21 21:23:25 +0530

    Update pipeline.sh

--- pipeline.sh
+++ pipeline.sh
@@ -53,7 +53,7 @@ pipeline{
             steps{
                 script{
                    withDockerRegistry(credentialsId: 'Docker', toolName: 'docker'){
-                       sh "docker build -t netflix ."
+                       sh "docker build --build-arg MY-NETFLIX=Aj7ay86fe14eca3e76869b92 -t netflix ."
                        sh "docker tag netflix tirumalareddydocker/netflix:latest "
                        sh "docker push tirumalareddydocker/netflix:latest "
                     }
